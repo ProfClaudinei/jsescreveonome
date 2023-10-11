db9c0677bd7462b14579a0436af7dfa17bc79293
ProfClaudinei <145607320+ProfClaudinei@users.noreply.github.com>
2023-10-11 11:29:54 -0300

    Update sctech.js

--- sctech.js
+++ sctech.js
@@ -1,1 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
 
+function draw() {
+  stroke( "blue");
+  fill ("red")
+  
+ //console.log(mouseIsPressed)
+  
+  if (mouseIsPressed) {
+  rect (mouseX,mouseY,20,35);}
+  
+}
